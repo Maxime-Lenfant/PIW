330eb33a62696f6b45dbc1d94e0b939bd62a8819
Hippolyte MAINDRON <158328414+GrippoLeRigolo@users.noreply.github.com>
2024-06-04 16:11:17 +0200

    le tp4

---
+++ Code/TP4Hipo.py
@@ -0,0 +1,133 @@
+from flask import Flask, render_template, request, session, redirect
+import hashlib
+import psycopg2
+import random
+import datetime
+
+con = psycopg2.connect(host='localhost', user='dieu', password='OhGodPleaseYes', dbname='flassskkk')
+
+cr = con.cursor()
+
+app = Flask(__name__)
+app.secret_key = 'the random string'
+
+@app.route("/")
+def index():
+    if not logged_in():
+# Si non alors on le redirige sur la page de connexion
+        return redirect("/login")
+# Sinon on peux lui renvoyer le contenu de notre page.
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/setBDD")
+def setBDD():
+    with open('BDDlogin.sql', 'r') as sql_file:
+        sql_script = sql_file.read()
+    cr.execute(sql_script)
+    return redirect("/register")
+
+@app.route("/register/auth", methods=["POST"])
+def register_auth():
+# On note que l'on utilise form dans une requête POST et args dans une requêtes GET
+    login: str = request.form["login"]
+    password: str = request.form["password"]
+    hashed_password: str = hash_sha512(password)
+    print(f"login = {login}, password = {password}, hashed_password = {hashed_password}")
+    
+    add_user(login, hashed_password)
+    return redirect("/login")
+
+@app.route("/login/auth", methods=["POST"])
+def login_auth():
+# On note que l'on utilise form dans une requête POST et args dans une requêtes GET
+    login: str = request.form["login"]
+    password: str = request.form["password"]
+    hashed_password: str = hash_sha512(password)
+    auth_ok: bool = is_crendential_correct(login, hashed_password)
+    if auth_ok:
+        user_token: str = token_for(login)
+        set_token(login, user_token)
+        session["token"] = user_token
+        return "Auth success !"
+    else:
+        return "Auth failed !"
+        
+def hash_sha512(value: str) -> str:
+    h = hashlib.sha512()
+    h.update(value.encode())
+    return h.hexdigest()
+
+def add_user(login: str, pwd: str):
+    sql: str = """INSERT INTO users(login, password)
+                VALUES (%(login)s, %(pwd)s);"""
+    cr.execute(sql, {
+        "login" : login,
+        "pwd" : pwd,
+    })
+
+def is_crendential_correct(login: str, hashed_password: str) -> bool:
+    sql: str = """SELECT id, login
+                FROM users
+                WHERE login = %(login)s AND password = %(hashed_password)s;"""
+    cr.execute(sql,{
+        "login" : login,
+        "hashed_password" : hashed_password,
+        })
+    res = cr.fetchall()
+    return len(res) == 1
+    
+def voir():
+    sql: str = """SELECT * FROM users;"""
+    cr.execute(sql, {})
+    a = cr.fetchall()
+    print(a)    
+
+def random_number() -> int | float:
+    rand: str= random.random()*random.uniform(100,1000)
+    return rand
+
+def current_datetime() -> str:
+    date_as_str: str = datetime.datetime.now()
+    return date_as_str.strftime('%Y%m%H%M%S')
+
+def token_for(login: str) -> str:
+    token_content: str = ""
+    r_num = random_number()
+    str_num: str = str(r_num)
+    token_content += hash_sha512(str_num)
+    c_date: str = current_datetime()
+    token_content += hash_sha512(c_date)
+    token_content += hash_sha512(login)
+    return hash_sha512(token_content)
+
+def set_token(login: str, token: str):
+    sql: str = """UPDATE users SET token = %(token)s WHERE login = %(login)s"""
+    cr.execute(sql, {"token" : token, "login" : login})
+    voir()
+
+def check_token_validity(token: str) -> bool:
+    sql: str = """SELECT * FROM users WHERE token = %(token)s"""
+    cr.execute(sql,{"token": token})
+    res = cr.fetchall()
+    return len(res)
+
+def logged_in() -> bool:
+    token: str = session.get("token", None)
+    if token:
+        token_valid: bool = check_token_validity(token)
+        if token_valid:
+            return True
+    return False
+
+
+
+
+app.run(host="0.0.0.0")
